54f318b1472000173bd63e8b65a901f8d306f48a
simonangel-fong <simonangelfong@gmail.com>
2026-08-11 11:27:34 -0400

    infra: notebook add

---
+++ infra/sagemaker-studio-notebook.tf
@@ -0,0 +1,38 @@
+# sagemaker-studio-notebook.tf
+
+# ##############################
+# Space: jupyterlab
+# ##############################
+resource "aws_sagemaker_space" "notebook" {
+  space_name = "admin-alice-jupyterlab"
+  domain_id  = aws_sagemaker_domain.yolo.id
+
+  space_sharing_settings {
+    sharing_type = "Private"
+  }
+
+  ownership_settings {
+    owner_user_profile_name = aws_sagemaker_user_profile.admin.user_profile_name
+  }
+
+  space_settings {
+    app_type = "JupyterLab"
+
+    jupyter_lab_app_settings {
+      default_resource_spec {
+        instance_type        = var.notebook_instance_type
+        lifecycle_config_arn = aws_sagemaker_studio_lifecycle_config.clone_repo.arn
+      }
+
+      code_repository {
+        repository_url = var.git_repository_url
+      }
+    }
+
+    space_storage_settings {
+      ebs_storage_settings {
+        ebs_volume_size_in_gb = var.notebook_volume_size
+      }
+    }
+  }
+}
